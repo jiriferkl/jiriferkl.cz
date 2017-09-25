ee54d3524c935dff95cab5d6cee6cfee79e09a97
Jiří Ferkl <jiri.ferkl@protonmail.com>
2017-09-25 19:45:01 +0200

    Install NPM

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+
+gulp.task('css', function () {
+	return gulp.src('./static/sass/*.sass')
+		.pipe(sass())
+		.pipe(minifyCss({compatibility: 'ie8'}))
+		.pipe(concat('dist.css'))
+		.pipe(gulp.dest('./static/dist'));
+});
+
+gulp.task('js', function () {
+	return gulp.src('./static/js/*.js')
+		.pipe(uglify())
+		.pipe(concat('bundle.js'))
+		.pipe(gulp.dest('./static/dist'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch(['./static/sass/*.sass'], ['css']);
+	gulp.watch(['./static/js/*.js'], ['js']);
+});
