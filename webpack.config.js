3efef5a460f729089315fc4008c76eca04028fbf
Jiří Ferkl <jiri.ferkl@protonmail.com>
2021-01-19 14:28:55 +0100

    Website: mass update

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'static/dist'),
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "style.css",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader'
+                ]
+            }
+        ]
+    }
+};
