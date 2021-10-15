a7865b5964b0cc38b6ca297aab71dbf0d2b866d3
Jiří Ferkl <jiri.ferkl@protonmail.com>
2021-10-15 14:27:31 +0200

    Resume: added

--- src/data.js
+++ src/data.js
@@ -8,6 +8,20 @@ export function loadAge() {
     setData("age", age);
 }
 
+export function loadStars() {
+    const x = document.getElementsByClassName('stars');
+    for (let i = 0; i < x.length; i++) {
+        const count = x[i].dataset.value;
+        for (let z = 0; z < count; z++) {
+            x[i].innerHTML += '<i class="fas fa-star" aria-hidden="true"></i>';
+        }
+
+        for (let z = count; z < 5; z++) {
+            x[i].innerHTML += '<i class="far fa-star" aria-hidden="true"></i>';
+        }
+    }
+}
+
 function setData(className, value) {
     const x = document.getElementsByClassName(className);
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import './theme.scss'
 import '@fortawesome/fontawesome-free/js/brands';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/regular';
 import { Dropdown } from 'bootstrap';
-import {loadExperienceYears, loadAge} from './data';
+import {loadExperienceYears, loadAge, loadStars} from './data';
 
 loadExperienceYears();
 loadAge();
+loadStars();
