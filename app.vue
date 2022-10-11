9b7316130664efc18479d2a80e56ddffa90d74a8
Jiří Ferkl <jiri.ferkl@protonmail.com>
2022-10-11 10:00:33 +0200

    Web: background styles

--- app.vue
+++ app.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
+  <div class="min-h-screen">
     <NavBar />
-    <NuxtPage/>
+    <NuxtPage />
+    <div id="backgroundDivider"></div>
   </div>
 </template>
+
+<style>
+body {
+  background: url("mountains.svg") no-repeat center bottom;
+  background-size: auto 50vh;
+}
+
+#backgroundDivider {
+  height: 52vh;
+}
+
+@media only screen and (min-width: 1728px) {
+  body {
+    background-size: 100vw 50vh;
+  }
+}
+</style>
