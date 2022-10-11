fbefd05d00ebf890fbef35b593bdb974b0bc907d
Jiří Ferkl <jiri.ferkl@protonmail.com>
2022-10-11 15:13:03 +0200

    Resume: added content

---
+++ components/ResumeImage.vue
@@ -0,0 +1,3 @@
+<template>
+  <img src="resume.jpg" class="rounded-full w-44 w-44">
+</template>
---
+++ server/api/education.ts
@@ -0,0 +1,49 @@
+export default defineEventHandler((event) => {
+    return [
+        {
+            "years": "2017 - 2017",
+            "place": "Opava",
+            "url": "http://www.slu.cz",
+            "title": "Silesian University",
+            "subtitle": "Information technology",
+            "description": [
+                "After the course I wanted to keep studying but also I wanted to have enough time for my work. So I studied IT remotely, but it didn't last long. I was pretty disappointed about it so instead of studying I decided to work full-time."
+            ],
+            "badges": [],
+            "logo": "schools/slu.jpg"
+        },
+        {
+            "years": "2016 - 2017",
+            "place": "Pardubice",
+            "title": "One-year English language course",
+            "subtitle": "B2/C1 level",
+            "description": [
+                "After graduation I wanted to have time for my personal project so I decided to take an English course that didn't take so much time.",
+                "The course ended with a state exam."
+            ],
+            "badges": [],
+            "logo": "schools/oapce.svg"
+        },
+        {
+            "years": "2012 - 2016",
+            "place": "Pardubice",
+            "url": "http://spse.cz/",
+            "title": "Secondary School Of Electrical Engineering",
+            "subtitle": "Information technology",
+            "description": [
+                "At primary school I realized that I love IT so when I had to choose what to do next, my choice was obvious.",
+                "I learned about Java, C# and PHP. Soon I figured out what the framework was and wanted to write one myself. Then I learned about Nette.",
+                "During my studies I was also on a foreign internship in Valencia, Spain."
+            ],
+            "badges": [
+                "Java",
+                "C#",
+                "PHP",
+                "Nette",
+                "Javascript",
+                "MySQL"
+            ],
+            "logo": "schools/spse.png"
+        }
+    ]
+})
