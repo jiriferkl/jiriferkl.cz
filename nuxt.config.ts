4c2052c7db733dc16b5b4664501cc3a7ea4cc46c
Jiří Ferkl <jiri.ferkl@protonmail.com>
2022-10-31 16:09:56 +0100

    App: fixed meta tag image urls

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,12 +17,12 @@ export default defineNuxtConfig({
                 { name: 'og:url', content: 'https://jiriferkl.cz/' },
                 { name: 'og:title', content: 'Jiří Ferkl - Full-stack web developer' },
                 { name: 'og:description', content: 'I\'m open for new challenges! PHP, JavaScript, MySQL, Docker, GraphQL, Vue.js, React, DevOps, Nette..' },
-                { name: 'og:image', content: '/ogimage.jpg' },
+                { name: 'og:image', content: 'https://jiriferkl.cz/ogimage.jpg' },
                 { name: 'twitter:card', content: 'summary_large_image' },
                 { name: 'twitter:url', content: 'https://jiriferkl.cz/' },
                 { name: 'twitter:title', content: 'Jiří Ferkl - Full-stack web developer' },
                 { name: 'twitter:description', content: 'I\'m open for new challenges! PHP, JavaScript, MySQL, Docker, GraphQL, Vue.js, React, DevOps, Nette..' },
-                { name: 'twitter:image', content: '/ogimage.jpg' },
+                { name: 'twitter:image', content: 'https://jiriferkl.cz/ogimage.jpg' },
             ]
         }
     },
