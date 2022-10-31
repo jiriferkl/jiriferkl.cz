feaadb6435f094a8972afecac930300f017b9373
Jiří Ferkl <jiri.ferkl@protonmail.com>
2022-10-31 15:58:40 +0100

    App: added meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,22 @@ export default defineNuxtConfig({
     css: [
         '@fortawesome/fontawesome-svg-core/styles.css'
     ],
+    app: {
+        head: {
+            title: 'Jiří Ferkl - Full-stack web developer',
+            meta: [
+                { name: 'description', content: 'I\'m open for new challenges! PHP, JavaScript, MySQL, Docker, GraphQL, Vue.js, React, DevOps, Nette..' },
+                { name: 'og:type', content: 'website' },
+                { name: 'og:url', content: 'https://jiriferkl.cz/' },
+                { name: 'og:title', content: 'Jiří Ferkl - Full-stack web developer' },
+                { name: 'og:description', content: 'I\'m open for new challenges! PHP, JavaScript, MySQL, Docker, GraphQL, Vue.js, React, DevOps, Nette..' },
+                { name: 'og:image', content: '/ogimage.jpg' },
+                { name: 'twitter:card', content: 'summary_large_image' },
+                { name: 'twitter:url', content: 'https://jiriferkl.cz/' },
+                { name: 'twitter:title', content: 'Jiří Ferkl - Full-stack web developer' },
+                { name: 'twitter:description', content: 'I\'m open for new challenges! PHP, JavaScript, MySQL, Docker, GraphQL, Vue.js, React, DevOps, Nette..' },
+                { name: 'twitter:image', content: '/ogimage.jpg' },
+            ]
+        }
+    },
 })
