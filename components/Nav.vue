83bab463ec920466d0ffe28c0329f51550a17565
Jiří Ferkl <jiri.ferkl@protonmail.com>
2022-10-31 14:02:28 +0100

    Nav: better link order

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <NuxtLink class="underline mr-3" to="/">Homepage</NuxtLink>
-    <NuxtLink class="underline mr-3" to="/about">About me</NuxtLink>
     <NuxtLink class="underline mr-3" to="/resume">Resume</NuxtLink>
-    <NuxtLink class="underline" to="/contact">Contact</NuxtLink>
+    <NuxtLink class="underline mr-3" to="/contact">Contact</NuxtLink>
+    <NuxtLink class="underline" to="/about">About me</NuxtLink>
   </div>
 </template>
