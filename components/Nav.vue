23d6c2bc74e993932dce155b06330b1a90df60e3
Jiří Ferkl <jiri.ferkl@protonmail.com>
2022-10-31 13:42:31 +0100

    Nav: added content

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <NuxtLink class="underline mr-3" to="/">Homepage</NuxtLink>
     <NuxtLink class="underline mr-3" to="/about">About me</NuxtLink>
     <NuxtLink class="underline mr-3" to="/resume">Resume</NuxtLink>
     <NuxtLink class="underline" to="/contact">Contact</NuxtLink>
