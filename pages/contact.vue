70ba2e08b6e737451dcaaeb6eab8f891275e51e8
Jiří Ferkl <jiri.ferkl@protonmail.com>
2022-10-31 13:58:25 +0100

    Contact: added content

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,3 +1,7 @@
 <template>
-  <h1>Contact page</h1>
+  <div class="flex flex-col items-center">
+    <h1 class="text-4xl mb-4">Get in Touch</h1>
+    <p class="text-2xl">You can reach me anytime on</p>
+    <p class="text-2xl"><a class="underline underline-offset-4" href="mailto:jiri.ferkl@protonmail.com">jiri.ferkl@protonmail.com</a></p>
+  </div>
 </template>
