65a47e30846e42bf5350612d0d6357d8bb7bc33f
Jiří Ferkl <jiri.ferkl@protonmail.com>
2022-11-18 11:20:45 +0100

    Pdf: implemented resume function

---
+++ pdf/api/resume.ts
@@ -0,0 +1,56 @@
+import {VercelRequest, VercelResponse} from '@vercel/node';
+import puppeteer from "puppeteer-core";
+import chromium from "chrome-aws-lambda";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+    const url: any = process.env.RESUME_URL;
+    if (!(url instanceof String)) {
+        throw Error('Required env is not set');
+    }
+
+    let browser = null;
+    let page = null;
+
+    try {
+        browser = await puppeteer.launch({
+            defaultViewport: {
+                width: 1920,
+                height: 1080,
+            },
+            args: chromium.args,
+            headless: chromium.headless,
+            executablePath: await chromium.executablePath,
+        });
+
+        page = await browser.newPage();
+        await page.goto(url, {waitUntil: 'networkidle0'});
+
+        const resume = await page.$("#resumeForPrint");
+        const box = await resume.boundingBox();
+
+        const pdf = await page.pdf({
+            printBackground: true,
+            width: box.width,
+            height: box.height - 79,
+        });
+
+        res.statusCode = 200;
+        res.setHeader("Content-Type", `application/pdf`);
+        res.end(pdf, 'binary');
+    } catch (e: any) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1>Server Error</h1><p>Internal server error</p>');
+
+        console.error(e);
+        console.error(e.message);
+    } finally {
+        if (page !== null) {
+            await page.close();
+        }
+
+        if (browser !== null) {
+            await browser.close();
+        }
+    }
+}
