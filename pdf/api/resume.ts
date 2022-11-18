5bcadd2c451adda9ee0fb386082503fa186a2d7d
Jiří Ferkl <jiri.ferkl@protonmail.com>
2022-11-18 12:20:07 +0100

    test

--- pdf/api/resume.ts
+++ pdf/api/resume.ts
@@ -4,6 +4,8 @@ import chromium from "chrome-aws-lambda";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     const url: any = process.env.RESUME_URL;
+    console.log(url);
+    console.log(url instanceof String);
     if (!(url instanceof String)) {
         throw Error('Required env is not set');
     }
