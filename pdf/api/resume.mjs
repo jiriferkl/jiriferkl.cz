272d6f7d370e2aabc079ae90ccc4bde04359ba87
Jiří Ferkl <jiri.ferkl@protonmail.com>
2024-04-27 16:04:03 +0200

    App: vercel pdf generator remove

---
+++ pdf/api/resume.mjs
@@ -0,0 +1,30 @@
+import puppeteer from "puppeteer";
+
+async function generateResume(url) {
+    const options = {
+        defaultViewport: {
+            width: 1920,
+            height: 1080,
+        },
+        headless: true,
+    };
+
+    const browser = await puppeteer.launch(options);
+    const page = await browser.newPage();
+
+    await page.goto(url, {waitUntil: 'networkidle0'});
+
+    const resume = await page.$("#resumeForPrint");
+    const box = await resume.boundingBox();
+    const pdf = await page.pdf({
+        printBackground: true,
+        width: box.width,
+        height: box.height - 21,
+        path: 'resume.pdf'
+    });
+
+    await page.close();
+    await browser.close();
+}
+
+generateResume("http://localhost:3000/resume/print");
